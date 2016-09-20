b7c7fad709ea375927aecf772890ed69de6e7fc9
Odirlei <Odirlei@Odirlei>
2016-09-20 14:30:26 -0300

    Primeiro commit

---
+++ src/gitteste/GitTeste.java
@@ -0,0 +1,14 @@
+
+package gitteste;
+
+/**
+ *
+ * @author Odirlei
+ */
+public class GitTeste {
+    
+    public static void main(String[] args) {
+    System.out.println("Sou foda!!!");
+    }
+    
+}
