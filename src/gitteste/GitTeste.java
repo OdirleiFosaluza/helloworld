a128b7e5ee66a64288f77ed4a310c698f696c80c
Odirlei <Odirlei@Odirlei>
2016-09-20 14:34:45 -0300

    Primeiro segundo commit teste

--- src/gitteste/GitTeste.java
+++ src/gitteste/GitTeste.java
@@ -9,6 +9,7 @@ public class GitTeste {
     
     public static void main(String[] args) {
     System.out.println("Sou foda!!!");
+    System.out.println("Será que sou foda mesmo ?");
     }
     
 }
